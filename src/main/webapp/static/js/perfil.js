c5a3c8eb42ae199f63a6ed637291c68412546b5e
Federico <toledofederico11@gmail.com>
2020-05-28 23:45:50 -0300

    agregado compra de bonos simbolica

--- src/main/webapp/static/js/perfil.js
+++ src/main/webapp/static/js/perfil.js
@@ -2,15 +2,16 @@ $(document).ready(function() {
     
     let cliente = getSession()
     
-    $(".id-cliente").text(cliente.idCliente)
-    $(".id-usuario").text(cliente.usuario)
-    $(".id-nombre").text(cliente.nombre)
-    $(".id-direccion").text(cliente.direccion)
+    //Muestro la informacion del cliente
+    setClienteInfo(cliente)
     
     let cuentas = getCuentas(cliente.idCliente);
     //Agrego las cuentas del cliente a los desplegables
     mostrarCuentas(cuentas);
+    //Agrego los bonos al desplegable
+    mostrarBonos()
     
+    //Realizar transferencia
     $("#realizar").click(function(e) {
         e.preventDefault();
                 
@@ -22,7 +23,7 @@ $(document).ready(function() {
         
         if(verificarSaldo(cuentaOrigen, monto)) {
             //Realizar transferencia
-            realizarTransferencia(cuentaOrigen, cuentaDestino, monto)
+            realizarTransaccion(cuentaOrigen, cuentaDestino, monto)
             
             //Actualizar saldos de las cuentas involucradas
             actualizarSaldo(cuentaActual, -monto)
@@ -35,23 +36,113 @@ $(document).ready(function() {
         
     });
     
+    //Realizar compra de bonos
+    $("#comprarBonos").click(function() {
+        let cuentaOrigen = $('#selectCuentaBono option').filter(':selected').val()
+        let idBono = $("#selectBono option").filter(':selected').val()
+        let cantidad = $("#cantidadBonos").val()
+        
+        let cuentaActual = cuentas.find(cuenta => cuenta.id == cuentaOrigen)
+        
+        let infoBono = getBono(idBono)
+        let monto = infoBono.precioCompra * cantidad
+        if(verificarSaldo(cuentaOrigen, monto)) {
+            realizarTransaccion(cuentaOrigen, 0, monto)
+            
+            actualizarSaldo(cuentaActual, -monto)
+            window.location.replace('transaccion-realizada.html')
+        } else {
+            $(".modal").addClass('is-active')
+        }
+        
+    })
+    
+    //Ver ultimos n movimientos 
+    $("#verUltimosMovimientos").click(function(){
+        let idCuenta = $('#ultimosMovSelect option').filter(':selected').val()
+        console.log(idCuenta)
+        let cantidad = parseInt($("#cantidad").val())
+        getUltimosMovimientos(idCuenta, cantidad)
+    })
+    
+    //Botones del menu lateral
     $("#btn-transferencia").click(function() {
         $("#transferencia").removeClass('no-mostrar')
+        $("#ultimos-movimientos").addClass('no-mostrar')
         $("#bonos").addClass('no-mostrar')
     })
     
     $("#btn-bonos").click(function() {
         $("#bonos").removeClass('no-mostrar')
+        $("#ultimos-movimientos").addClass('no-mostrar')
+        $("#transferencia").addClass('no-mostrar')
+    })
+    
+    $("#btn-ultimos-movimientos").click(function() {
+        $("#ultimos-movimientos").removeClass('no-mostrar')
+        $("#bonos").addClass('no-mostrar')
         $("#transferencia").addClass('no-mostrar')
     })
     
 })
 
+function setClienteInfo(cliente) {
+    $(".id-cliente").text(cliente.idCliente)
+    $(".id-usuario").text(cliente.usuario)
+    $(".id-nombre").text(cliente.nombre)
+    $(".id-direccion").text(cliente.direccion)
+}
+
+function getUltimosMovimientos(idCuenta, cantidad) {
+    $.ajax({
+        url: 'http://localhost:8080/Banco-FVM/rest/transacciones/ultimas/'+idCuenta+'/'+cantidad,
+        contentType: 'application/json',
+        dataType: 'json',
+        type: 'get',
+        success: function(movimientos) {
+            for(m in movimientos) {
+                let mov = movimientos[m]
+                console.log(mov.fecha)
+                let htmlAppend = '\
+                    <br><div class="card">\n\
+                        <div class="card-header">\n\
+                            <div class="card-header-title">Movimiento del '+mov.fecha+'</div>\n\
+                        </div>\n\
+                        <div class="card-content">\n\
+                            <div class="content">\n\
+                                <p>Origen: '+mov.cuentaOrigen+'</p>\n\
+                                <p>Destino: '+mov.cuentaDestino+'</p>\n\
+                                <p>Monto: '+mov.monto+'</p></div>\n\
+                            </div>\n\
+                        </div>\n\
+                    </div>'
+                $(".lista-ultimos-mov").append(htmlAppend)
+            }
+        }
+    })
+}
+
 function mostrarCuentas(cuentas) {
     for(c in cuentas) { //c es el indice
         let option = new Option(cuentas[c].aliasCuenta,cuentas[c].id); 
-        $('#cuentas,#cuentasBonos').append($(option));
+        $('#cuentas,#selectCuentaBono,#ultimosMovSelect').append($(option));
     }
+}
+
+function mostrarBonos() {
+    $.ajax({
+        url: 'http://localhost:8080/Banco-FVM/rest/bonos',
+        type: 'get',
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function(bonos) {
+            for(b in bonos) {
+                let bono = bonos[b]
+                let option = new Option(bono.nombre+' - $'+bono.precioCompra,bono.id); 
+                $('#selectBono').append($(option));
+            }
+        }
+    })
 }
 
 function getSession() {
@@ -87,7 +178,7 @@ function verificarSaldo(origen, monto) {
     return suficiente
 }
 
-function realizarTransferencia(origen, destino, monto) {
+function realizarTransaccion(origen, destino, monto) {
 
     let transferencia = {
         "cuentaOrigen": origen,
@@ -128,6 +219,21 @@ function getCuentas(idCliente) {
         }
     })
     return cuentas;
+}
+
+function getBono(idBono) {
+    let bono;
+    $.ajax({
+        url: 'http://localhost:8080/Banco-FVM/rest/bonos/'+idBono,
+        type:'get',
+        contentType: 'application/json',
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            bono = data
+        }
+    })
+    return bono;
 }
 
 function actualizarSaldo(cuenta, diferencia) {
